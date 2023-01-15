057d483ab80eb86932b5ee6d623302c4f3925ff5
Rahul Sharma <er.csrahul@gmail.com>
2023-01-15 22:20:09 +0530

    Initial commit

---
+++ test/arc-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ArcCdk from '../lib/arc-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/arc-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ArcCdk.ArcCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
